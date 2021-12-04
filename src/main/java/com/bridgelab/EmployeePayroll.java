76d9df37a2c533149d579b03d9bfbd30797bd13b
Antara055 <antara55.dgp10.1434@gmail.com>
2021-12-04 13:22:38 +0530

    Added: database connection

---
+++ src/main/java/com/bridgelab/EmployeePayroll.java
@@ -0,0 +1,32 @@
+package com.bridgelab;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.Enumeration;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        String dbURl="jdbc:mysql://localhost:3306/payroll_service";
+        String userName="Antara";
+        String password="##Antara55";
+        try {
+            Connection connection = DriverManager.getConnection(dbURl, userName, password);
+            if (connection != null) {
+                System.out.println("Connected"+connection);
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        listDrivers();
+    }
+    public static void listDrivers(){
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }
+}
